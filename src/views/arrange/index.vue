8f64fbfc4dcb4985e4d63e132689d1e87af08fb0
chenx@dtinone.com <2c1a5590cc61f120b391967ed50955b6>
2020-09-23 17:49:23 +0800

    add

--- src/views/arrange/index.vue
+++ src/views/arrange/index.vue
@@ -6,7 +6,7 @@
 import { Component, Vue } from "vue-property-decorator";
 
 @Component
-export default class Arrage extends Vue {}
+export default class ClassName extends Vue {}
 </script>
 
 <style lang="less" scope>
