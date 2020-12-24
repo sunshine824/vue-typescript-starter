efd6fcd3f5b3a360fea349275bac36b8dd570171
陈鑫 <chenxin@chenxindeMacBook-Pro.local>
2020-12-24 17:54:37 +0800

    解决axios “AxiosResponse<any>”上不存在属性“XXXX” 问题

--- src/types/axios.d.ts
+++ src/types/axios.d.ts
@@ -1,7 +1,6 @@
 import * as axios from 'axios'
 
 declare module 'axios' {
-  interface AxiosInstance {
-    (config: AxiosRequestConfig): Promise<any>
-  }
+  //解决axios “AxiosResponse<any>”上不存在属性“XXXX” 问题
+  export interface AxiosResponse<T = any> extends StoreState.Result { }
 }
